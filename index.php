495cf41050d224828c73d780a6ecb4837e10966b
egik <mas.awe.awe@gmail.com>
2016-07-27 16:17:50 +0700

    dunno

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+?>
+
+
+<?php
+// session_start();
+// if(isset($_SESSION["user"])){
+//   echo "Selamat datang, ".$_SESSION["user"]."</br>";
+//   echo "Password anda adalah: ".substr_replace($_SESSION["pass"], "*****", 0, -2)."";
+// }
+// else{
+  ?><!DOCTYPE html>
+  <html>
+  <body>
+    <p>Selamat Datang silahkan <a href="login.php">login</a></p>
+  </body>
+  </html><?php
+};
+
+ ?>
+
+<?php
+//session_destroy();
+?>
